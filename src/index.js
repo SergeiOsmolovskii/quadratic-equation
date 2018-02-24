f9b0024dc8ac6fcc16537a46e0a6f535d88a72aa
SergeiOsmolovskii <36640948+SergeiOsmolovskii@users.noreply.github.com>
2018-02-24 14:45:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+ var massiv=[];
+ let massivrezult=[];
+ let a, b, c, D ,x1 ,x2;
+massiv=equation.split(' ');1
+a=(massiv[0]);
+b=(massiv[4]);
+c=(massiv[8]);
+if (massiv[0]==='-')a=-a;
+if (massiv[3]==='-')b=-b;
+if (massiv[7]==='-')c=-c;
+D = Math.pow(b,2) - 4 * a * c;
+if (D>0){
+x1 = Math.round((-1 * b + Math.sqrt(D)) / (2 * a));
+x2 = Math.round((-1 * b - Math.sqrt(D)) / (2 * a));
+if (x1<x2)
+  massivrezult.push(x1, x2);
+else
+  massivrezult.push(x2, x1);
+}
+ return massivrezult;
+
 }
